0a17bd288021ef459878b8c7d33055c28664329c
Sarah Stephens <sarahstephens84@gmail.com>
2022-09-25 17:00:40 -0400

    commit tree b6fceddb76910bf6946431cd446d8581dcaad2e3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+  // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayDrivers){
+    return (arrayDrivers.slice(0,2))
+}
+
+const returnLastTwoDrivers = function(arrayDrivers){
+    return (arrayDrivers.slice(-2))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(ride) {
+    return function(fare) {
+        return ride * fare;
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+//const fareTripler(fare) => fare(3);
+
+/*const selectDifferentDrivers = function(selection){
+    if selection = 1{
+        return returnFirstTwoDrivers
+    } else if selection = 2{
+        return returnLastTwoDrivers;
+    }
+}
+selectDifferentDrivers(1)*/
+
+
+function selectDifferentDrivers(arrayOfDrivers, chooseBetweenDrivers) {
+    return chooseBetweenDrivers(arrayOfDrivers);
+}
+console.log(chooseBetweenDrivers(arrayDrivers,returnFirstTwoDrivers))
+console.log(chooseBetweenDrivers(arrayDrivers, returnLastTwoDrivers))
+
